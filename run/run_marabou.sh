8ec7d333865139fd263b43030cd93871c1f3160d
j29scott <j29scott@uwaterloo.ca>
2021-07-30 08:05:06 -0400

    fix

--- run/run_marabou.sh
+++ run/run_marabou.sh
@@ -2,4 +2,5 @@ file_dir="$(dirname "$0")"
 cd $file_dir
 cd ..
 . .env.d/openenv.sh
-cd $file_dirpython3 -m dnnv $1 --network N $2 --marabou --vnnlib
+cd $file_dir
+python3 -m dnnv $1 --network N $2 --marabou --vnnlib
